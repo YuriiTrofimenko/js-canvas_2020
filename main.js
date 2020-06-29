84bcf17da567e91e4ca0b72d3355e988613b7d4f
YuriiTrofimenko <tyaa@ukr.net>
2020-06-29 10:32:13 +0300

    lineTo, arc, bezier, path

--- main.js
+++ main.js
@@ -10,6 +10,37 @@ if (canvas.getContext) {
   ctx.strokeRect (55, 55, 100, 100)
   ctx.clearRect (55, 55, 100, 100)
   ctx.clearRect (54, 54, 102, 102)
+  ctx.beginPath()
+  ctx.moveTo(55,55)
+  ctx.lineTo(75,100)
+  ctx.lineTo(100,75)
+  ctx.fill()
+  ctx.strokeStyle = "#900";
+  ctx.stroke()
+  ctx.beginPath()
+  ctx.arc(75, 75, 50, 0, Math.PI * 2, true) // x, y, radius, startAngle, endAngle, anticlockwise
+  ctx.fill()
+  ctx.beginPath()
+  ctx.moveTo(110, 110)
+  ctx.fillStyle = "green"  
+  ctx.arcTo(125,200, 200,150, 50)
+  ctx.fill()
+  ctx.beginPath()
+  ctx.moveTo(50, 20)
+  ctx.lineWidth = 5
+  ctx.quadraticCurveTo(230, 30, 50, 100)
+  ctx.stroke()
+  ctx.beginPath()
+  ctx.moveTo(50,20)
+  ctx.lineWidth = 5
+  ctx.bezierCurveTo(230, 30, 150, 60, 50, 100)
+  ctx.stroke()
+  const rectangle = new Path2D()
+  rectangle.rect(110, 110, 150, 150)
+  const circle = new Path2D()
+  circle.moveTo(225, 135)
+  ctx.stroke(rectangle)
+  ctx.fill(circle)
 } else {
   console.log('Canvas is not supported in this browser')
 }
