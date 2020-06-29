f7a67ca96bfb4b48d425091342807d852c67599f
YuriiTrofimenko <tyaa@ukr.net>
2020-06-29 09:50:23 +0300

    alpha channel, strokeRect, clearRect

--- main.js
+++ main.js
@@ -5,6 +5,11 @@ if (canvas.getContext) {
   const ctx = canvas.getContext("2d")
   ctx.fillStyle = "#900"
   ctx.fillRect(10, 10, 100, 100)
+  ctx.fillStyle = "rgba(0, 0, 200, 0.5)"
+  ctx.fillRect (55, 55, 100, 100)
+  ctx.strokeRect (55, 55, 100, 100)
+  ctx.clearRect (55, 55, 100, 100)
+  ctx.clearRect (54, 54, 102, 102)
 } else {
   console.log('Canvas is not supported in this browser')
 }
